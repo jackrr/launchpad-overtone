ab7b42987a67a9facf8f083f6bde2d08a34471fd
Jack Ratner <jackratner@gmail.com>
2025-09-26 17:20:50 -0400

    messages receiving and sending
    Turns out launchpad attaches as 2 Midi devices, one for DAW, the other
    for programmer mode. The java midi library doesn't seem to pick up on
    any difference when device labeling, and the overtone API greedily
    picks the first match.
    
    Required copying over some fns from overtone to pick the last
    device. I'll follow up with the ability to filter again by name..
    
    Successfully sending LED controls and receiving pad presses!

--- src/launchpad_overtone/core.clj
+++ src/launchpad_overtone/core.clj
@@ -1,25 +1,88 @@
 (ns launchpad-overtone.core)
 
 (use 'overtone.core)
-(use 'overtone.live)
+;; (use 'overtone.midi)
+;; (use 'overtone.live)
 
-;; (require '[overtone.midi :as midi])
-(require '[overtone.studio.midi :as midis])
-(require '[overtone.inst.drum :as drum])
+(require '[overtone.midi :as midi])
+;; (require '[overtone.inst.drum :as drum])
 
-;; (midi/midi-sources)
-;; (midi/midi-find-device "Launchpad")
-(def device (midis/midi-find-connected-devices "Launchpad"))
-(def receiver (midis/midi-find-connected-receivers "Launchpad"))
+(def illumination-kinds
+  {:led/static 0
+   ;; NOTE: Seemingly cannot transition from pulse or static to flash
+   :led/flash  1
+   :led/pulse  2})
+
+;; https://github.com/josephwilk/overtone.device.launchpad/blob/master/src/launchpad/device.clj
+;; https://userguides.novationmusic.com/hc/en-gb/articles/24001475492498-Controlling-the-Launchpad-X-surface
+
+(defn init-midi-devices []
+  ;; (let [
+  ;;       ;; pad-in (midi/midi-in "Launchpad") 
+  ;;       ;; pad-out (midi/midi-out "Launchpad")
+  ;;       ]
+  ;;   {:in pad-in :out pad-out})
+  {:in (-> (midi/midi-sources) last with-transmitter)
+   :out (-> (midi/midi-sinks) last with-receiver)}
+  )
+
+(defn- with-receiver
+  "Add a midi receiver to the sink device info. This is a connection
+   from which the MIDI device will receive MIDI data"
+  [sink-info]
+  (println sink-info)
+  (let [^javax.sound.midi.MidiDevice dev (:device sink-info)]
+    (if (not (.isOpen dev))
+      (.open dev))
+    (assoc sink-info :receiver (.getReceiver dev))))
+
+(defn- with-transmitter
+  "Add a midi transmitter to the source info. This is a connection from
+   which the MIDI device will transmit MIDI data."
+  [source-info]
+  (let [^javax.sound.midi.MidiDevice dev (:device source-info)]
+    (if (not (.isOpen dev))
+      (.open dev))
+    (assoc source-info :transmitter (.getTransmitter dev))))
 
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn illuminate
+  "Trigger LED of kind on device at slot with given color"
+  [device button kind color]
+  (let [msg (javax.sound.midi.ShortMessage.)]
+    (.setMessage msg javax.sound.midi.ShortMessage/CONTROL_CHANGE (kind illumination-kinds) button color)
+    (midi/midi-send-msg (:receiver device) msg 0)))
 
 (comment
-  (do
-    (boot-external-server)
-    (init))
-     )
+
+  (boot-external-server)
+
+  (let [{:keys [in out]} (init-midi-devices)]
+    (midi/midi-handle-events in
+                           (fn [msg] (println "GOT MSG:" msg))
+                           (fn [msg]
+                             (println "GOT SYSEX MSG:" msg)
+                             (-> msg :data vec println)))
+    
+    )
+
+  (let [{:keys [in out]} (init-midi-devices)]
+    (illuminate out 56 :led/static 0)
+    )
+
+  
+
+  
+
+  (let [msg (javax.sound.midi.SysexMessage.)]
+    ;; This might be device inquiry message
+    ;; (.setMessage msg (byte-array [240 126 127 6 1 247]) 6)
+    ;; This is programmer/live mode layout
+    (.setMessage msg (byte-array [240 0 32 41 2 12 0 127 247]) 9)
+    ;; This is note mode layout
+    ;; (.setMessage msg (byte-array [240 0 32 41 2 12 0 1 247]) 9)
+    (midi/midi-send-msg (:receiver lpout) msg -1)
+    )
+
+
+  )
