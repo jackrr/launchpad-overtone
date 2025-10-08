d740f24c28769f98c17b2302892df933359e928e
Jack Ratner <jackratner@gmail.com>
2025-10-08 14:49:41 -0400

    wip

---
+++ src/launchpad_overtone/modes/looper.clj
@@ -0,0 +1,12 @@
+(ns launchpad-overtone.modes.looper
+  (:require [launchpad-overtone.pad :as pad]))
+
+;; TODO:
+;; time/metronome visual
+;; recording
+;; - schedule vs immediate recording start
+;; - visual feedback of recording modes
+;; play/pause/stop playback
+;; loop length programming
+;; record over vs playback
+;; multiple registers on pad
